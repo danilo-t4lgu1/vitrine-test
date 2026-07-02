7d162763ed0a29b5ed9c438d6e26abb9ed8a2c83
danilo-t4lgu1 <danilo.prado@talgui.com.br>
2026-07-02 13:19:32 -0300

    Create vitrine-vestidos-test.js

---
+++ vitrine-vestidos-test.js
@@ -0,0 +1,4 @@
+(function() {
+  if (!window.LS || !window.LS.category || window.LS.category.id !== 36839648) return;
+  console.log('[vitrine-spike-test] executado na categoria Vestidos', window.LS.category);
+})();
